e753356a0aefffe80f13deff152db6abaa022c25
Hung Nguyen <huuhung10082003@gmail.com>
2025-09-30 12:44:40 +0700

    library

---
+++ src/library/entity/TaiLieuEntity.java
@@ -0,0 +1,18 @@
+package library.entity;
+
+public class TaiLieu {
+    protected String ma;
+    protected String ten;
+    protected int nam;
+    protected String tacGia;
+
+    public TaiLieu() {};
+
+    public TaiLieu(String ma, String ten,int nam,String tacGia){
+        this.ma = ma;
+        this.ten = ten;
+        this.nam = nam;
+        this.tacGia= tacGia;
+    }
+    
+}
